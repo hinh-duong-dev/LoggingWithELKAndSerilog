21d7b3e28448d92acc8c4eadccf5d829b8ac7aea
hinhduong <duonghinh0402@gmail.com>
2022-09-28 19:35:18 +0700

    Add project files.

---
+++ InventoryService/Program.cs
@@ -0,0 +1,29 @@
+using BuyService.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace InventoryService
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                //.WriteTo.File("F:\\elk_log.txt")
+                .WriteTo.Http("http://localhost:9601/")
+                .Enrich.CustomerLogEnricher()
+                .CreateLogger();
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
